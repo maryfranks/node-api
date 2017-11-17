6246b46d1b736dfe4c63cf7fa58d8c16b58922e2
maryfranks <maryfranks@gmail.com>
2017-11-17 08:51:58 -0500

    add url to filter instock items

--- api.js
+++ api.js
@@ -2,9 +2,26 @@ const http = require("http");
 const data = require("./data/inventory");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/json"});
-  response.end(JSON.stringify(data));
+
+  if (request.url === "/") {
+    response.writeHead(200, {"Content-Type": "text/json"});
+    response.end(JSON.stringify(data));
+  } else if (request.url === "/instock") {
+    listInStock(response);
+  }
+
+
 
 }).listen(3000);
 
 console.log("listening on port 3000");
+
+function listInStock(response) {
+
+  var inStock = data.filter(function(item) {
+    return item.avail === "In stock";
+  });
+
+  response.end(JSON.stringify(inStock));
+
+}
