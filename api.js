46cb053efaed2b720befb3ebe9c36649f92b3548
maryfranks <maryfranks@gmail.com>
2017-11-17 08:43:37 -0500

    serve json data with server

--- api.js
+++ api.js
@@ -2,8 +2,9 @@ const http = require("http");
 const data = require("./data/inventory");
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World");
+  response.writeHead(200, {"Content-Type": "text/json"});
+  response.end(JSON.stringify(data));
+
 }).listen(3000);
 
 console.log("listening on port 3000");
